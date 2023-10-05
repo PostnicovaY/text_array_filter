2f446e44171b4c18664c041bb5a5345abb65bafa
PostnicovaY <oceana91@rambler.com>
2023-10-05 22:59:25 +0300

    Wrote a program that solved the problem, adjusted the flowchart and README file

---
+++ text_array_filter/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача: Написать программу, которая из имеющегося массива 
+// строк формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется 
+// пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+string[] array = {"Hello", "2", "world", ":-)"};
+string[] newArray = textArrayFilter(array);
+
+string[] textArrayFilter(string[] array){
+    int count = 0;
+    int maxLength = 3;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= maxLength){
+            count++;
+        }
+    }
+    string[] newArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= maxLength){
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
+Console.Write(String.Join(" ",newArray));
